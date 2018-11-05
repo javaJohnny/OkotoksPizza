096bdd47c7e6f7a6d797ff3efdaae3de8d910123
John Medina <johnrae0109@shaw.ca>
2018-11-05 11:20:15 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:image" content="images/okotoks-pizza-logo.png" />
+    <meta property="og:title" content="Okotoks Best Pizza Place" />
+    <meta property="og:description" content="#1 Pizza dough in the country!, also try our Salads and Pasta! GMO FREE!" />
+    <meta property="og:url" content="http//okotokspizza.com" />
+
+    <meta name="twitter:title" content="Okotoks Pizza, Pastas, & Salads" />
+    <meta name="twitter:description" content="#1 Pizza dough in the country!, also try our Salads and Pasta! GMO FREE!" />
+    <meta name="twitter:image" content="images/okotoks-pizza-logo.png" />
+    <meta name="twitter:card" content="summary-large-image" />
+
+    <link rel="shortcut icon" type="image/png" href="images/shortcut-icon.png" />
+
+
+    <script src="js/swiper.min.js"></script>
+
+    <link rel="stylesheet" href="css/layout.css" type="text/css">
+    <link rel="stylesheet" href="css/bulma.css" type="text/css">
+    <link rel="stylesheet" href="css/swiper.min.css" type="text/css">
+    <link rel="stylesheet" href="webfonts/css/fontawesome-all.css" />
+
+    <title>Home|Okotoks Pizza - Pasta - Salad</title>
+</head>
+
+<body>
+    <header class="is-mobile is-tablet is-desktop">
+        <div id="logo">
+            <a href="index.html"><img src="images/logo.png" alt="logo" /></a>
+        </div>
+        <h1 class="toggle">Okotoks Pizza, Best Pizza, Pasta, Salad in the world! Amazing Quality dough!</h1>
+        <h1>BEST PIZZA IN THE WORLD. HAVE YOU TASTED BETTER?</h1>
+        <div class="nav">
+            <div class="columns is-desktop">
+                <div class="column">
+                    <p>
+                        <a href="offers.html">OFFERS</a>
+                    </p>
+                </div>
+                <div class="column">
+                    <p>
+                        <a href="menu.html">MENU</a>
+                    </p>
+                </div>
+                <div class="column">
+                    <p>
+                        <a href="about.html">ABOUT</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section class="wrapper">
+
+        <h2>SO WHAT WILL IT BE?</h2>
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <div class="product">
+                        <h3>8" Ala Mo Wana</h3>
+                        <img src="images/pizza/bronco-pizza.png" alt="Ala Mo Wana"/>
+                        <h4><span class="hot">HOT</span><br /><span class="dollar">$</span>7.62</h4>
+                        <h4><span class="cold">COLD</span><br /><span class="dollar">$</span>7.00</h4>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="product">
+                        <h3>8" Egg n Bacon</h3>
+                        <img src="images/pizza/bronco-pizza.png" alt="Egg n Bacon" />
+                        <h4><span class="hot">HOT</span><br /><span class="dollar">$</span>7.14</h4>
+                        <h4><span class="cold">COLD</span><br /><span class="dollar">$</span>6.50</h4>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="product">
+                        <h3>8" San Pedro Egg</h3>
+                        <img src="images/pizza/bronco-pizza.png" alt="San Pedro Egg" />
+                        <h4><span class="hot">HOT</span><br /><span class="dollar">$</span>7.14</h4>
+                        <h4><span class="cold">COLD</span><br /><span class="dollar">$</span>6.50</h4>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="product">
+                        <h3>8" Bacon Double Cheese</h3>
+                        <img src="images/pizza/bronco-pizza.png" alt="Bacon Double Cheese" />
+                        <h4><span class="hot">HOT</span><br /><span class="dollar">$</span>7.62</h4>
+                        <h4><span class="cold">COLD</span><br /><span class="dollar">$</span>7.00</h4>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="product">
+                        <h3>8" Big Al's Pizza</h3>
+                        <img src="images/pizza/bronco-pizza.png" alt="Big Al's Pizza" />
+                        <h4><span class="hot">HOT</span><br /><span class="dollar">$</span>8.10</h4>
+                        <h4><span class="cold">COLD</span><br /><span class="dollar">$</span>7.25</h4>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="product">
+                        <h3>8" Bootlegger</h3>
+                        <img src="images/pizza/bronco-pizza.png" alt="Bootlegger" />
+                        <h4><span class="hot">HOT</span><br /><span class="dollar">$</span>7.14</h4>
+                        <h4><span class="cold">COLD</span><br /><span class="dollar">$</span>6.50</h4>
+                    </div>
+                </div>
+            </div>
+
+            <div class="swiper-pagination"></div>
+            <!-- Arrows -->
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+
+            <script>
+            var swiper = new Swiper('.swiper-container', {
+              slidesPerView: 3,
+              spaceBetween: 30,
+              slidesPerGroup: 3,
+              loop: true,
+              loopFillGroupWithBlank: true,
+              pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+              },
+              navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+              },
+            });
+            </script>
+        </div>
+    </section>
+    <section class="container" class="whitebg">
+        <div class="columns is-8">
+            <div class="column" id="dine-in">
+                <h2>DINE-IN</h2>
+                <p>
+                    <span class="strong">116</span> Elizabeth St, Okotoks, AB T1S 1J9
+                </p>
+                <div class="map-responsive">
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5051.467560477013!2d-113.97628931666229!3d50.72487794500796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5d5c5e58aa73ada7!2sGreat+Canadian+Pizza!5e0!3m2!1sen!2sca!4v1529341767582" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="column" id="hours">
+                <h2>HOURS</h2>
+                <p>
+                    <span class="strong">MONDAY:</span> 8:00AM - 8:00PM
+                    <br />
+                    <span class="strong">TUESDAY:</span> 8:00AM - 8:00PM
+                    <br />
+                    <span class="strong">WEDNESDAY:</span> 8:00AM - 8:00PM
+                    <br />
+                    <span class="strong">THURSDAY:</span> 8:00AM - 8:00PM
+                    <br />
+                    <span class="strong">FRIDAY:</span> 8:00AM - 9:00PM
+                    <br />
+                    <span class="strong">SATURDAY:</span> 9:00AM - 8:00PM
+                    <br />
+                    <span class="strong">SUNDAY:</span> ALWAYS--CLOSED
+                    <br />
+                </p>
+            </div>
+        </div>
+    </section>
+    <footer class="level">
+        <div class="level left">
+            <div class="level-item">
+              <a href="tel:1-403-555-5555" class="is-hidden-desktop"><i class="fas fa-phone"></i>(403)-555-5555</a>
+              <span class="is-hidden-touch"><i class="fas fa-phone copyright">(403)-555-5555</i></span>
+            </div>
+        </div>
+        <div class="level-right">
+            <div class="level-item">
+                <p class="copyright">
+                    Copyright &copy; 2018 Okotoks Pizza
+                </p>
+            </div>
+        </div>
+
+    </footer>
+</body>
+
+</html>
